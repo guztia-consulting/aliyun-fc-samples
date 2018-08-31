e8531f0d88cf84e1eab8bccbd3c5ee1e6b2cb82d
Alberto Roura <mail@albertoroura.com>
2018-09-01 09:06:45 +1000

    Update StopInstance.js

--- StopInstance.js
+++ StopInstance.js
@@ -1,20 +1,17 @@
 const ALY = require('aliyun-sdk')
 
-const handler = (event, context, callback) => {
-    const { credentials, region } = context
-    const { accessKeyId, accessKeySecret, securityToken } = credentials
-    const ecsArgs = JSON.parse(event)
+const accessKeyId = '_accessKeyId_'
+const secretAccessKey = '_secretAccessKey_'
+
+exports.handler = (event, context, callback) => {
+    const args = JSON.parse(event)
 
     const ecs = new ALY.ECS({
         accessKeyId,
-        secretAccessKey: accessKeySecret,
+        secretAccessKey,
         endpoint: 'https://ecs.aliyuncs.com',
         apiVersion: '2014-05-26',
-        region,
-        securityToken,
     })
 
-    ecs.stopInstance(ecsArgs, callback)
+    ecs.stopInstance(args, callback)
 }
-
-module.exports = { handler }
