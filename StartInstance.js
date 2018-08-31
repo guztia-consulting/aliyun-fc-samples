e36a436467a7d6409871deaad8ff5083c323dc6d
Alberto Roura <mail@albertoroura.com>
2018-09-01 09:07:36 +1000

    Create StartInstance.js

---
+++ StartInstance.js
@@ -0,0 +1,17 @@
+const ALY = require('aliyun-sdk')
+
+const accessKeyId = '_accessKeyId_'
+const secretAccessKey = '_secretAccessKey_'
+
+exports.handler = (event, context, callback) => {
+    const args = JSON.parse(event)
+
+    const ecs = new ALY.ECS({
+        accessKeyId,
+        secretAccessKey,
+        endpoint: 'https://ecs.aliyuncs.com',
+        apiVersion: '2014-05-26',
+    })
+
+    ecs.startInstance(args, callback)
+}
